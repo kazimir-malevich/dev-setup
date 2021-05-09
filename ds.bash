0c240b036204d6b14283c5112587b956280ec2e4
kazimir-malevich <adrian_lewis@hushmail.com>
2021-05-09 16:03:02 +0100

    refactor

--- ds.bash
+++ ds.bash
@@ -2,38 +2,38 @@
 
 set -e
 
-declare -A arr
-
-setup() {
+setup_all() {
   sudo apt -y update && sudo apt -y upgrade
-  rm -rf "$HOME/.vim"
+  rm -rf $HOME/.vim
 }
 
-git() {
+setup_git() {
   sudo apt install git
   wget -O $HOME/.gitconfig https://raw.githubusercontent.com/kazimir-malevich/dotfiles/master/.gitconfig
 }
 
-vim() {
-  plugins_start_dir="$HOME/.vim/pack/plugins/start"
+setup_vim() {
+  plugins_start_dir="$HOME/.vim/pack/plugins/start/"
+
   sudo apt install vim
   wget -O $HOME/.vimrc https://raw.githubusercontent.com/kazimir-malevich/dotfiles/master/.vimrc
-  mkdir -p $plugins_start_dir
 
-  arr["ale"]="dense-analysis/ale.git"
-  arr+=( ["vim-commentary"]="tpope/vim-commentary" ["ctrlp.vim"]="ctrlpvim/ctrlp.vim.git" )
+  mkdir -p $plugins_start_dir && cd $plugins_start_dir
 
-  for key in ${!arr[@]}; do
-    echo ${key} ${arr[${key}]}
-    git clone "https://github.com/${arr[${key}]}" "${plugins_start_dir}/${key}"
+  declare -a plugins
+  plugins=("dense-analysis/ale.git" "tpope/vim-commentary" "ctrlpvim/ctrlp.vim.git")
+  cd $plugins_start_dir
+
+  for i in ${plugins[@]}; do
+    git clone "https://github.com/${i}"
   done
 }
 
-teardown() {
+teardown_all() {
   sudo apt -y autoremove
 }
 
-setup
-git
-vim
-teardown
+setup_all
+setup_git
+setup_vim
+teardown_all
